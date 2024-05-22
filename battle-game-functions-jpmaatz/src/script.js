cc1f81388e65bc9f68fb3028ed8049ad45c8674c
jpmaatz <97722319+jpmaatz@users.noreply.github.com>
2024-05-22 17:28:38 -0500

    Add files via upload

--- battle-game-functions-jpmaatz/src/script.js
+++ battle-game-functions-jpmaatz/src/script.js
@@ -13,33 +13,28 @@ const logHealth = (player,health) => console.log("" + player + " health " + heal
 
 const logDeath = (winner,loser) => console.log("" + winner + " defeated " + loser + "!")
 
-const isDead = health => health <= 0 ? true:false 
+const isDead = health => health <= 0 ? true : false 
 
 function fight (player1,player2,player1Health,player2Health){
+    while(true){
+        const attacker = chooseOption
     
-    const attacker = chooseOption
-    while(player1 || player2 = chooseOption){
-        return true;
+        if (attacker === player1){
+            player2Health = attackPlayer(player2Health);
+            console.log(logHealth(player2, player2Health))
+            if (isDead(player2Health)){
+                logDeath(player2, player1);
+                break;
+            }
+        } else {
+            player1Health = attackPlayer(player1Health);
+            console.log(logHealth(player1, player1Health))
+            if (isDead(player2Health)){
+                logDeath(player1, player2);
+                break;
+            }
+        }
     }
-
-    if (attacker = player1){
-        return player2Health = attackPlayer(player2Health);
-    }
-    console.log(logHealth(player2,player2Health))
-
-    if (isDead(player2Health)){
-        return logDeath(player1,player2);
-        break;
-    }
-
-} else (player1Health === attackPlayer(player1Health)) {
-    return logHealth(player1,player1Health);
-
-    if (isDead(player1Health)){
-        return logDeath(player2,player1);.
-        break;
-    }
-
 }
 
-fight(Mitch,Adam,100,100)
+fight("Mitch", "Adam", 100, 100)
